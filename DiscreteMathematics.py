3c6e4f74b98dac684aa0147363ca5ff25752326b
Nihil K'ri <NihilKri@live.com>
2024-08-10 03:19:58 -0500

    Basic Euclid's Algorithm

--- DiscreteMathematics.py
+++ DiscreteMathematics.py
@@ -1,1 +1,19 @@
 
+
+def GCD(x: int, y: int, dbug: bool = False) -> int:
+  """ Find the Greatest Common Divisor using Euclid's Algorithm. """
+  # x and y are positive integers where x >= y
+  if(x <= 1 or y <= 1): return 1
+  a, b = (int(x), int(y)) if (x >= y) else (int(y), int(x))
+  r, g = 1, 0
+  while(r > 0):
+    r = a % b
+    d = a // b
+    if(dbug): print(f"{a} = {d} * {b} + {r}")
+    
+    if(r>0): a,b = b,r
+  if(dbug): print(f"The GCD of {x} and {y} is {b}.")
+  return b
+
+
+GCD(156, 54, True)
